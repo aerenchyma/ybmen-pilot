db49e70b409e6142d95f26a3117f917471fc3a78
jczetta <jczetta@gmail.com>
2014-09-23 12:48:31 -0400

    models updates (still not succssful participant)

--- ybmenpilot/ybmenpilot/models.py
+++ ybmenpilot/ybmenpilot/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Update(models.Model):
 	post_id = models.CharField(primary_key=True,max_length=100)
 	content = models.TextField(default="")
-	date_posted =  models.TextField(max_length=100)
-	time_posted = models.CharField(max_length=25)
-	link = models.CharField(max_length=200)
+	date_posted =  models.TextField(max_length=100,default="")
+	time_posted = models.CharField(max_length=25,default="")
+	link = models.CharField(max_length=200,default="")
 	person_id = models.CharField(max_length=100)
 	content_type = models.CharField(max_length=25)
 	imagecontent = models.CharField(max_length=1000)
@@ -15,7 +15,7 @@ class Update(models.Model):
 	application = models.CharField(max_length=200,default="")
 
 class Participant(models.Model):
-	ident = models.CharField(primary_key=True, max_length=200)
+	ident = models.CharField(primary_key=True,max_length=200)
 	name = models.CharField(max_length=80,default="")
 	token = models.CharField(max_length = 4000)
 	birthday = models.CharField(max_length=12, default="") # how are we getting this info, bday or age range
@@ -26,9 +26,9 @@ class Participant(models.Model):
 
 class GroupPost(models.Model):
 	ident = models.CharField(primary_key=True,max_length=100)
-	content = models.TextField()
-	date_posted = models.DateField()
-	time_posted = models.CharField(max_length=25)
+	content = models.TextField(default="")
+	date_posted = models.CharField(max_length=25,default="")#models.DateField()
+	time_posted = models.CharField(max_length=25,default="")
 	num_comments_recd = models.IntegerField(default=0,editable=True)
 	num_likes_recd = models.IntegerField(default=0)
 	num_shares = models.IntegerField(default=0) # hm
